7b0b4cc0235342b1ce8791fa5b9c004b4a9cf56a
bharathi-025 <bharathisivakumar25@gmail.com>
2024-11-09 15:47:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to the Developer Cards App</h1>
-      <p>To add your details, please submit a PR with your information in the <code>developers.json</code> file.</p>
+      <p>22CB011</p>
       <CardList />
     </div>
   );
